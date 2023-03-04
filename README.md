e6c4535c856bc8fa16382a7d579f2d0e44e2153c
Skonik <skonik@evrone.com>
2023-03-04 20:40:03 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# flake8-drf
+
+Flake8 plugin that detects some issues with your drf code.
+Work in progress. Currently detects places where you should use drf status constants.
+
+## Installation
+
+```console
+pip install flake8-drf
+```
+
+
+## Usage
+Let's say we have the following code below:
+
+```python
+from rest_framework import views
+from rest_framework.views import Response, APIView
+
+
+class AuthView(APIView):
+
+    def get(self, request):
+        return Response(status=200)
+
+
+class PostsView(views.APIView, APIView):
+
+    def post(self, request):
+        return Response(status=201)
+```
+
+Running flake8 with the plugin will tell the following:
+```console
+main.py:8:25: DRF0 Status code can be changed to constant: HTTP_200_OK
+main.py:14:25: DRF0 Status code can be changed to constant: HTTP_201_CREATED
+```
---
+++ flake8_drf/plugin.py
@@ -0,0 +1,74 @@
+import ast
+from _ast import Call
+from typing import Any, Generator, List, Tuple, Type, NamedTuple, Union
+
+from flake8_drf.constants import STATUS_CODES, DRF_RESPONSE_CLASS_NAME, DRF_RESPONSE_STATUS_KEYWORD
+
+
+class Flake8Error(NamedTuple):
+    lineno: int
+    col_offset: int
+    msg: str
+
+
+class Visitor(ast.NodeVisitor):
+    parent = None
+
+    def __init__(self) -> None:
+        self.errors: List[Flake8Error] = []
+
+    def visit(self, node):
+        node.parent = self.parent
+        self.parent = node
+
+        return super().visit(node)
+
+    def _check_error(
+            self,
+            status_code: int,
+            arg: Union[ast.arg, ast.keyword],
+    ) -> str:
+        if status_code in STATUS_CODES.keys():
+            change_to = STATUS_CODES[status_code]
+            error = Flake8Error(
+                arg.lineno,
+                arg.col_offset,
+                f'DRF0 Status code can be changed to constant: {change_to}',
+            )
+            self.errors.append(error)
+
+    def _check_positional_args(self, node):
+        if node.args:
+            arg = node.args[0]
+            if isinstance(arg, ast.Constant):
+                status_code = arg.value
+                self._check_error(status_code, arg)
+
+    def _check_status_keyword_args(self, node):
+        for keyword in node.keywords:
+            if keyword.arg == DRF_RESPONSE_STATUS_KEYWORD:
+                if isinstance(keyword.value, ast.Constant):
+                    status_code = keyword.value.value
+                    self._check_error(status_code, keyword)
+
+    def visit_Call(self, node: Call) -> Any:
+        if isinstance(node.func, ast.Name):
+            callable_name = node.func.id
+            if callable_name == DRF_RESPONSE_CLASS_NAME:
+                self._check_positional_args(node)
+                self._check_status_keyword_args(node)
+
+
+class Plugin:
+    name = 'flake8-drf'
+    version = '0.1.0'
+
+    def __init__(self, tree: ast.AST):
+        self._tree = tree
+
+    def run(self) -> Generator[Tuple[int, int, str, Type[Any]], None, None]:
+        visitor = Visitor()
+        visitor.visit(self._tree)
+
+        for error in visitor.errors:
+            yield error.lineno, error.col_offset, error.msg, type(self)
